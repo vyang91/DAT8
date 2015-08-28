976aef9d9676d21a052f1a611b186bb62f5b7c52
Victor Yang <victoryang1@gmail.com>
2015-08-28 06:07:42 -0400

    modified hw3 answers

--- homework/03_answers.py
+++ homework/03_answers.py
@@ -8,7 +8,9 @@ Created on Fri Aug 28 12:14:02 2015
 # %reset
 import csv
 with open('chipotle.tsv', 'rU') as f:
-    file_nested_list = [row for row in csv.reader(f, delimiter='\t')]
+    dialect = csv.Sniffer().sniff(f.read(1024))
+    f.seek(0)
+    file_nested_list = [row for row in csv.reader(f, dialect)]
 
 header = file_nested_list[0]
 data = file_nested_list[1:]
@@ -27,14 +29,11 @@ count_burrito_topping = [i.count(',') + 1 for i in lst_burrito_topping]
 avg_burrito_topping = float(sum(count_burrito_topping))/len(lst_burrito_topping)
 
 lst_chip_names = set([item_order[2] for item_order in data if item_order[2].lower().find('chip') >= 0])
-
 lst_chip_count = []
-        
 for chip_name in lst_chip_names:
     int_chip_count = 0
     for item_order in data:
         if item_order[2] == chip_name:
             int_chip_count += 1 * int(item_order[1])
     lst_chip_count.append(int_chip_count)
-    
 dict_chip_count = dict(zip(lst_chip_names, lst_chip_count))
